cb53793bc5cd1aadba7a136004effef2ea8e2b2f
carolinezhao <chaohsimeng@gmail.com>
2017-10-21 23:08:22 +0800

    add tests for functions

--- DOM Scripting/DOM-imagegallery.js
+++ DOM Scripting/DOM-imagegallery.js
@@ -1,21 +1,32 @@
 // 使用到的方法：getElementById, getElementsByTagName，getAttribute, setAttribute
 // 使用到的属性：nodeValue, firstChild
 
-// 函数目标：点击某链接时，占位符替换为该链接要显示的图片。
+// 函数目标：点击某链接时，占位符替换为该链接要显示的图片。 
 function showImg(targetImg) {
+    // 
+    if (!document.getElementById("placeholder")) return false;
     // 函数的唯一参数为带有href属性的元素节点<a>，将其href属性值存储在source变量中。
     var source = targetImg.getAttribute("href");
     // 用其它图片替换占位符img，需要改变img的src属性，通过id获取该元素节点。
     var placeholder = document.getElementById("placeholder");
     placeholder.setAttribute("src", source);
 
-    // 将图片的title属性值存储在text变量中
-    // 当某个图片链接被点击时，这个链接的title属性值被提取并保存到text变量中。
-    var text = targetImg.getAttribute("title");
-    var description = document.getElementById("description");
-    // 获得id为description的p元素节点的第一个子节点－文本节点－的nodeValue属性值，并设置为变量text的值。
-    description.firstChild.nodeValue = text;
+    // 将描述部分放在if语句里后，description元素是可选的。如果存在则被更新，否则就被忽略。
+    if (document.getElementById("description")) {
+        // 将图片的title属性值存储在text变量中
+        // 当某个图片链接被点击时，这个链接的title属性值被提取并保存到text变量中。
+        var text = targetImg.getAttribute("title");
+        var description = document.getElementById("description");
+        // 获得id为description的p元素节点的第一个子节点－文本节点－的nodeValue属性值，并设置为变量text的值。
+        description.firstChild.nodeValue = text;
+    }
+    return true;
 }
+// ＊＊＊补充测试和检查＊＊＊
+// showImg负责完成两件事，第一件事是这个函数必须完成的任务，第二件事是锦上添花。
+// 检查工作希望达成的效果：只要placeholder图片存在，即使description元素不存在，切换显示新图片的操作也将照常进行，因此对两者采用不同的检查方法。
+
+
 
 // 改进版本：行为层js与结构层html分离
 // 函数目标：为带有特定id的链接添加onclick事件，将操作关联到onclick事件上。
@@ -32,10 +43,15 @@ function prepareGallery() {
     // links是一个节点列表 node list，这个集合里的每个节点都有自己的属性和方法。
     // 通过遍历获得每个节点，设置onclick事件，让它在有关链接被点击时完成以下操作：
     // 1.把链接作为参数传递给showImg；2.取消默认行为
-    for (var i = 0; i < links.length; i++) { 
+    for (var i = 0; i < links.length; i++) {
         links[i].onclick = function () {
-            showImg(this);
-            return false;
+            // showImg(this);
+            // return false;
+            // ＊＊＊补充测试和检查＊＊＊
+            // 以上两个语句使用的前提是showImg函数会正常返回。但如果把placeholder图片删掉，则点击任何链接都没有响应。
+            // 应该使浏览器能打开被点击的链接（平稳退化）。因此是否要返回一个false值以取消onclick的默认行为，应该由showImg函数决定。
+            // showImg有两种可能的返回值：图片切换成功，返回true；切换失败，返回false。对showImg的返回值取反，从而决定了是否阻止默认行为。
+            return !showImg(this);
         }
     }
 }
@@ -63,9 +79,9 @@ function addLoadEvent(func) {
     // 如果还没有绑定函数，就直接把新函数添加给它
     if (typeof window.onload != 'function') {
         window.onload = func;
-    // 如果已经绑定了函数，则把新函数追加到已有指令的末尾
+        // 如果已经绑定了函数，则把新函数追加到已有指令的末尾
     } else {
-        window.onload = function() {
+        window.onload = function () {
             oldonload();
             func();
         }
