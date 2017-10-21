946f264a45ebe4f5044745ece4176a453528c7f0
carolinezhao <chaohsimeng@gmail.com>
2017-10-21 16:05:34 +0800

    update window.onload and its best solution

--- DOM Scripting/DOM-imagegallery.js
+++ DOM Scripting/DOM-imagegallery.js
@@ -17,6 +17,7 @@ function showImg(targetImg) {
     description.firstChild.nodeValue = text;
 }
 
+// 改进版本：行为层js与结构层html分离
 function prepareGallery() {
     if (!document.getElementsByTagName) return false;
     if (!document.getElementById) return false;
@@ -31,4 +32,34 @@ function prepareGallery() {
     }
 }
 
-window.onload = prepareGallery;
+// prepareGallery函数中涉及对DOM元素的检查，因此需要函数在页面全部加载完毕后执行。
+// window.onload = prepareGallery;
+
+// 当有两个函数都需要在页面加载时执行，如果逐一绑定到onload事件上，则只有最后的那个会被实际执行。
+// 可以使用一个匿名函数来容纳这两个函数，再绑定到onload事件上，就都可以顺利执行。
+// window.onload = function() {
+//     firstFunction();
+//     secondFunction();
+// }
+
+// window.onload 最佳解决方案
+// 唯一参数：打算在页面加载完毕时执行的函数
+function addLoadEvent(func) {
+    // 现有window.onload事件处理函数的值存入变量
+    var oldonload = window.onload;
+    // 如果还没有绑定函数，就直接把新函数添加给它
+    if (typeof window.onload != 'function') {
+        window.onload = func;
+    // 如果已经绑定了函数，则把新函数追加到已有指令的末尾
+    } else {
+        window.onload = function() {
+            oldonload();
+            func();
+        }
+    }
+}
+// 把页面加载完毕时要执行的函数创建为一个队列。当代码比较复杂时，这个函数尤其实用，只需要添加如下语句
+// addLoadEvent(firstFunction);
+// addLoadEvent(secondFunction);
+// 本案例中需要执行的函数暂时只有 prepareGallery
+addLoadEvent(prepareGallery);
