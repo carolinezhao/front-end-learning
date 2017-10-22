5c885f7623fd6e3536ec11f8d565905fa912b76d
carolinezhao <chaohsimeng@gmail.com>
2017-10-22 17:27:04 +0800

    add more tests and comments

--- DOM Scripting/DOM-imagegallery.js
+++ DOM Scripting/DOM-imagegallery.js
@@ -1,32 +1,39 @@
 // 使用到的方法：getElementById, getElementsByTagName，getAttribute, setAttribute
-// 使用到的属性：nodeValue, firstChild
+// 使用到的属性：nodeValue, firstChild，nodeName, nodeType
 
-// 函数目标：点击某链接时，占位符替换为该链接要显示的图片。 
+// ＊＊＊ 补充测试和检查：减少对标记的依赖和假设 ＊＊＊
+// showImg负责完成两件事，第一件事是这个函数必须完成的任务，第二件事是锦上添花。
+// 检查工作希望达成的效果：只要placeholder图片存在，即使description元素不存在，切换显示新图片的操作也将照常进行，因此对两者采用不同的检查方法。
+// 操作：在showImg中增加不同类型的if判断语句。
+// PS：这里是为了学习检查的方法，实际工作中，并不是每一步都需要检查。对于html可能不在控制范围内的情况，需要检查。
+
+// 函数目标：点击某链接时，占位符替换为该链接要显示的图片，占位文本替换为链接的title属性值。
 function showImg(targetImg) {
-    // 
+    // 检查placeholder图片是否存在。
     if (!document.getElementById("placeholder")) return false;
     // 函数的唯一参数为带有href属性的元素节点<a>，将其href属性值存储在source变量中。
     var source = targetImg.getAttribute("href");
     // 用其它图片替换占位符img，需要改变img的src属性，通过id获取该元素节点。
     var placeholder = document.getElementById("placeholder");
+    // 检查placeholder是否是一张图片。
+    if (placeholder.nodeName != "IMG") return false;
     placeholder.setAttribute("src", source);
 
-    // 将描述部分放在if语句里后，description元素是可选的。如果存在则被更新，否则就被忽略。
+    // 检查占位文本是否存在。将描述部分放在if语句里，description元素是可选的。如果存在则被更新，否则就被忽略。
     if (document.getElementById("description")) {
-        // 将图片的title属性值存储在text变量中
         // 当某个图片链接被点击时，这个链接的title属性值被提取并保存到text变量中。
-        var text = targetImg.getAttribute("title");
+        // var text = targetImg.getAttribute("title");
+        // 检查：如果title存在，则赋值给text；否则，将空字符串赋值给text。
+        var text = targetImg.getAttribute("title") ? targetImg.getAttribute("title") : "";
         var description = document.getElementById("description");
-        // 获得id为description的p元素节点的第一个子节点－文本节点－的nodeValue属性值，并设置为变量text的值。
-        description.firstChild.nodeValue = text;
+        // 检查firstChild是否为文本节点。
+        if (description.firstChild.nodeType == 3) {
+            // 获得id为description的p元素节点的第一个子节点－文本节点－的nodeValue属性值，并设置为变量text的值。
+            description.firstChild.nodeValue = text;
+        }
     }
     return true;
 }
-// ＊＊＊补充测试和检查＊＊＊
-// showImg负责完成两件事，第一件事是这个函数必须完成的任务，第二件事是锦上添花。
-// 检查工作希望达成的效果：只要placeholder图片存在，即使description元素不存在，切换显示新图片的操作也将照常进行，因此对两者采用不同的检查方法。
-
-
 
 // 改进版本：行为层js与结构层html分离
 // 函数目标：为带有特定id的链接添加onclick事件，将操作关联到onclick事件上。
@@ -47,12 +54,16 @@ function prepareGallery() {
         links[i].onclick = function () {
             // showImg(this);
             // return false;
-            // ＊＊＊补充测试和检查＊＊＊
+            // ＊＊＊ 补充测试和检查 ＊＊＊
             // 以上两个语句使用的前提是showImg函数会正常返回。但如果把placeholder图片删掉，则点击任何链接都没有响应。
             // 应该使浏览器能打开被点击的链接（平稳退化）。因此是否要返回一个false值以取消onclick的默认行为，应该由showImg函数决定。
-            // showImg有两种可能的返回值：图片切换成功，返回true；切换失败，返回false。对showImg的返回值取反，从而决定了是否阻止默认行为。
+            // showImg中图片切换成功，返回true，应阻止默认行为；图片切换失败，返回false，应允许默认行为。因此对showImg的返回值取反作为返回值。
             return !showImg(this);
+            // 或者使用三元运算符
+            // return showImg(this) ? false : true;
         }
+        // 让键盘事件与onclick事件触发同样的行为。但一般浏览器都会默认触发onlick事件，且onkeypress事件很容易出问题，因此无需额外声明。
+        // links[i].onkeypress = links[i].onclick;
     }
 }
 // 结构化程序设计 structed programming 有一条原则：函数应该只有一个入口和一个出口。
