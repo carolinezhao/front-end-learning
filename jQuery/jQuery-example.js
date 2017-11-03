d730434207e38338fc3c5048761964d5062485fe
carolinezhao <chaohsimeng@gmail.com>
2017-11-03 23:20:39 +0800

    an example of jQuery

---
+++ jQuery/jQuery-example.js
@@ -0,0 +1,13 @@
+function main() {
+    $('.skillset').hide();
+    $('.skillset').fadeIn(1000);
+    $('.projects').hide();
+    $('.projects-button').on('click', function () {
+        //$(this).next().toggle();
+        $(this).next().slideToggle(400);
+        $(this).toggleClass('active');
+        $(this).text('Projects Viewed');
+    });
+}
+
+$(document).ready(main);
