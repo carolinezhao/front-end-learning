8c3f4c70247a6a80bfe53d44266c9a68805bcc52
carolinezhao <chaohsimeng@gmail.com>
2017-12-07 23:50:35 +0800

    private method

--- js/object/JS22-object-private.js
+++ js/object/JS22-object-private.js
@@ -2,28 +2,49 @@
 // Public means that they can be accessed outside the class.
 
 // Private Variables
-// Private variables are pieces of information you do not want to publicly share, and they can only be directly accessed from within the class.
+// can only be directly accessed from within the class.
 // Accessing Private Variables
-// We can define a public method that returns the value of a private variable.
+// An object's private variables can only be accessed by public methods that are part of that same object.
+
+// Private Methods
+// Methods can also be private within a class and inaccessible outside of the class. 
+// Access a private method
+// is similar to accessing a private variable. You must create a public method for the class that returns the private method.
+// 在一个class中，定义内部变量，从外部无法直接获取；但是可以在class中定义一个全局method，去拿到内部变量的值，然后在外部通过调用这个method去获得内部变量的值。
 
 function Person(first, last, age) {
     this.firstName = first;
     this.lastName = last;
     this.age = age;
-    // Private Variables    
+    // private variable
     var bankBalance = 888;
-    // Accessing Private Variables
-    this.getBalance = function() {
-        return bankBalance;
+    // access a private variable
+    this.getBalance = function (password) {
+        if (password === 1234) return bankBalance;
+        else return 'Wrong password!';
+    }
+
+    var address = 'Beijing';
+    // private method  
+    var getAddress = function () {
+        return address;
+    }
+    // access a private method
+    this.askAddress = function () {
+        // This means that it returns the method itself and NOT the result of calling that method. So you should NOT have parentheses.
+        return getAddress;
     }
 }
-// 在一个class中，定义内部变量，从外部无法直接获取；但是可以在class中定义一个全局method，去拿到内部变量的值，然后在外部通过调用这个method去获得内部变量的值。
 
 var rabbit = new Person('caroline', 'zhao', 25);
 console.log(rabbit.firstName);
 console.log(rabbit.lastName);
 console.log(rabbit.bankBalance); //undefined
 console.log(rabbit.getBalance()); //888
+// console.log(rabbit.getAddress(); //error
 
-
-
+// Because askAddress returns a method, we need to call it to make it any use. () - () - string
+var getAddressMethod = rabbit.askAddress();
+console.log(getAddressMethod); //function
+var myAddress = getAddressMethod();
+console.log(myAddress);
