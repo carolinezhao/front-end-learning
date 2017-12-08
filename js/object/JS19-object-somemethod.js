f2138708f3baed6dcfd8f09383bab56015e0c657
carolinezhao <chaohsimeng@gmail.com>
2017-12-08 23:46:33 +0800

    for/in loop

--- js/object/JS19-object-somemethod.js
+++ js/object/JS19-object-somemethod.js
@@ -28,16 +28,32 @@ if (info.hasOwnProperty('gender')) {
     console.log(info.gender);
 } else {
     info.gender = 'female';
-    console.log(info.gender);    
+    console.log(info.gender);
 }
 console.log('\n');
 
 // List all the Properties
 // by assigning the property name to a variable, we can then use the variable name in bracket notation to get the property's value.
+// This will go through all the properties of obj one by one and assign the property name to key on each run of the loop.
 for (key in info) {
     console.log(key);
 }
 console.log('\n');
 for (key in info) {
     console.log(info[key]);
+}
+console.log('\n');
+
+var languages = {
+    English: 'Hello!',
+    French: 'Bonjour!',
+    notLanguage: 2,
+    Spanish: 'Hola!'
+}
+
+// print hello in the 3 different languages
+for (var key in languages) {
+    if (typeof languages[key] === 'string') {
+        console.log(languages[key]);
+    }
 }
