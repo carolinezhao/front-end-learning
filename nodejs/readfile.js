9dbf382f9d873391cfca3f38f2a3aaedf4edbd1e
carolinezhao <chaohsimeng@gmail.com>
2017-12-17 01:09:26 +0800

    update comments

--- nodejs/readfile.js
+++ nodejs/readfile.js
@@ -1,5 +1,8 @@
 // 用异步 async 的方式读取文件，通过回调函数实现。
-var fs = require('fs');
+var fs = require('fs'); // require 到的 fs 是什么？
+// console.log(typeof fs); // object
+// console.log(fs);
+
 // readFile 读取了三个参数：文件名，编码方式，回调函数
 fs.readFile('file.txt', 'utf-8', function (err, data) {
     if (err) {
@@ -25,3 +28,6 @@ console.log('end.');
 // 运行结果：
 // end.
 // contents of the file
+
+// fs.readFile 调用时所做的工作只是将异步式I/O请求发送给了操作系统，然后立即返回并执行后面的语句，执行完后进入事件循环监听事件。
+// 当 fs 接收到I/O请求完成的事件时，事件循环会主动调用回调函数以完成后续工作。
