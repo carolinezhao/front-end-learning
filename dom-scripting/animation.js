9318a3278eed3dc6b904c0e18407e8cc3e1c53da
carolinezhao <chaohsimeng@gmail.com>
2017-12-18 00:21:03 +0800

    fix bug

--- dom-scripting/animation.js
+++ dom-scripting/animation.js
@@ -28,43 +28,45 @@ parseFloat(string); 可以返回带小数点的数值（浮点数）
 =========================================*/
 
 function positionMessage() {
+    console.log('hello positonMessage');
     if (!document.getElementById) return false;
-    if (document.getElementById("message")) return false;
+    if (!document.getElementById("message")) return false;
     var elem = document.getElementById("message");
     elem.style.position = 'absolute';
     elem.style.top = '50px';
     elem.style.left = '100px';
     // 全局变量，不使用关键字 var，意味着这个行为可以在函数以外的地方被取消。
     // 注意 setTimeout 中对函数名的引用
-    movement = setTimeout("moveMessage()", 5000); // 改进为渐变动画后，此句注释
+    movement = setTimeout("moveMessage()", 5000);
 }
 
 function moveMessage() {
+    console.log('hello moveMessage');  
     if (!document.getElementById) return false;
-    if (document.getElementById("message")) return false;
+    if (!document.getElementById("message")) return false;
     var elem = document.getElementById("message");
-    // msg.style.left = '200px'; // 使元素直接跳到目标位置
+    // elem.style.left = '200px'; // 使元素直接跳到目标位置
     // 将坐标字符串转换为数值，便于之后进行数值比较。50px -> 50
     var xpos = parseInt(elem.style.left);
     var ypos = parseInt(elem.style.top);
-    if (xpos == 200 && ypos == 100) {
+    if (xpos == 300 && ypos == 200) {
         return true; // 如果到达位置，则退出函数。
     }
-    if (xpos < 200) {
+    if (xpos < 300) {
         xpos++;
     }
-    if (xpos > 200) {
+    if (xpos > 300) {
         xpos--;
     }
-    if (ypos < 100) {
+    if (ypos < 200) {
         ypos++;
     }
-    if (ypos > 100) {
+    if (ypos > 200) {
         ypos--;
     }
     elem.style.left = xpos + 'px';
     elem.style.top = ypos + 'px';
-    movement = setTimeout("moveMessage()", 10); // 在短暂的停顿之后重复执行函数
+    movement = setTimeout("moveMessage()", 20); // 在短暂的停顿之后重复执行函数
 }
 
 
@@ -82,6 +84,6 @@ function addLoadEvent(func) {
 }
 
 addLoadEvent(positionMessage);
-addLoadEvent(moveMessage);
+// addLoadEvent(moveMessage);
 
 // clearTimeout(movement); 应该放在哪里用？？？
