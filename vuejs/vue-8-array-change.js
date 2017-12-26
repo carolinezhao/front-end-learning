e8b18901149a3fa2606035c043076b687b154b09
carolinezhao <chaohsimeng@gmail.com>
2017-12-26 23:21:29 +0800

    v-for on template / with v-if / for filtering

--- vuejs/vue-8-array-change.js
+++ vuejs/vue-8-array-change.js
@@ -7,13 +7,28 @@ var vm = new Vue({
             todo: 'coding'
         }, {
             todo: 'review'
-        }]
+        }],
+        numbers: [1, 2, 3, 4, 5]
     },
-    methods:{
-        replace:function() {
+    methods: {
+        replace: function () {
             // 非变异方法
-            this.items = this.items.filter(function(item) {
+            this.items = this.items.filter(function (item) {
                 return item.todo.match(/coding/) // 这是啥意思？
+            })
+        },
+        // 被筛选数组作为参数调用
+        even:function(numbers) {
+            return numbers.filter(function (number) {
+                return number % 2 === 0
+            })
+        }
+    },
+    computed:{
+        // 通过 this 使用被筛选数组
+        evenNumbers:function(){
+            return this.numbers.filter(function(number) {
+                return number % 2 === 0
             })
         }
     }
