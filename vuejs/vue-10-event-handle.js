39ab00b2be6cd663489f70f992b3b91f99203e4b
carolinezhao <chaohsimeng@gmail.com>
2017-12-27 23:28:24 +0800

    solve questions

--- vuejs/vue-10-event-handle.js
+++ vuejs/vue-10-event-handle.js
@@ -6,9 +6,12 @@ var vm = new Vue({
     },
     methods: {
         greet: function (event) {
+            // 如果不传参数，可通过此途径定义
+            // let event = window.event
             alert('Hello ' + this.name + '!')
             // event 是原生 DOM 事件
             if (event) {
+                // 事件绑定的元素
                 alert(event.target.tagName)
             }
         },
@@ -16,8 +19,8 @@ var vm = new Vue({
             alert(message)
         },
         warn: function (message, event) {
-            // 现在可以访问原生事件对象？？？
             if (event) {
+                // 阻止元素发生默认的行为（例如，当点击提交按钮时阻止对表单的提交）。这个 event 参数来自事件绑定函数。                
                 event.preventDefault()
             }
             alert(message)
