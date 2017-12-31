b786db59687049a9eeb3c416f3809d579476782c
carolinezhao <chaohsimeng@gmail.com>
2017-12-31 23:02:25 +0800

    prop

--- vuejs/vue-12-component.js
+++ vuejs/vue-12-component.js
@@ -29,17 +29,37 @@ Vue.component('simple-counter', {
     }
 })
 // 反例：如果不是函数会怎样
-var data = {
-    counter: 0
-}
-Vue.component('simple-counter', {
-    template: '<button v-on:click="counter +=1">{{counter}}</button>',
-    // 技术上 data 的确是一个函数了，因此 Vue 不会警告，但却给每个组件实例返回了同一个对象的引用
-    data: function () {
-        return data
-    }
-})
+// var data = {
+//     counter: 0
+// }
+// Vue.component('simple-counter', {
+//     template: '<button v-on:click="counter +=1">{{counter}}</button>',
+//     // 技术上 data 的确是一个函数了，因此 Vue 不会警告，但却给每个组件实例返回了同一个对象的引用
+//     data: function () {
+//         return data
+//     }
+// })
 // 三个组件实例共享了同一个 data 对象，因此递增一个 counter 会影响所有组件
+
+
+
+
+// 组件组合
+// 父子组件的关系：父组件通过 prop 给子组件下发数据，子组件通过事件给父组件发送消息。
+
+// 使用 Prop 传递数据
+// 组件实例的作用域是孤立的。这意味着不能 (也不应该) 在子组件的模板内直接引用父组件的数据。父组件的数据需要通过 prop 才能下发到子组件中。
+// HTML 特性是不区分大小写的。所以，当使用的不是字符串模板时，camelCase (驼峰式命名) 的 prop 需要转换为相对应的 kebab-case (短横线分隔式命名)。
+// 在 JavaScript 中使用 camelCase, 在 HTML 中使用 kebab-case
+Vue.component('child',{
+    // 用 props 选项声明它预期的数据
+    props:['myMessage'],
+    // 就像 data 一样，prop 也可以在模板中使用，同样也可以在 vm 实例中通过 this.message 来使用
+    template:'<span>{{myMessage}}</span>'
+})
+
+
+
 
 
 
@@ -53,6 +73,7 @@ var vm = new Vue({
     }
 })
 
+// 在哪儿注册为子实例？
 var vm2 = new Vue({
     el: '#container',
     components: {
