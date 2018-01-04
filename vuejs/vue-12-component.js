ab104cc93b1679d2c8502337c7ef1f3c2aec1c62
carolinezhao <chaohsimeng@gmail.com>
2018-01-05 01:24:43 +0800

    dynamic prop

--- vuejs/vue-12-component.js
+++ vuejs/vue-12-component.js
@@ -1,5 +1,6 @@
 // 组件可以扩展 HTML 元素，封装可重用的代码。在较高层面上，组件是自定义元素，Vue.js 的编译器为它添加特殊功能。
 // 所有的 Vue 组件同时也都是 Vue 的实例，所以可接受相同的选项对象 (除了一些根级特有的选项) 并提供相同的生命周期钩子。
+// "Vue实例是Vue应用的启动器，Vue组件是Vue实例的扩展。"
 
 // DOM 模板解析注意事项
 // 当使用 DOM 作为模板时 (例如，使用 el 选项来把 Vue 实例挂载到一个已有内容的元素上)，会受到 HTML 本身的一些限制。
@@ -59,12 +60,25 @@ Vue.component('simple-counter', {
 // 组件实例的作用域是孤立的。这意味着不能 (也不应该) 在子组件的模板内直接引用父组件的数据。父组件的数据需要通过 prop 才能下发到子组件中。
 // HTML 特性是不区分大小写的。所以，当使用的不是字符串模板时，camelCase (驼峰式命名) 的 prop 需要转换为相对应的 kebab-case (短横线分隔式命名)。
 // 在 JavaScript 中使用 camelCase, 在 HTML 中使用 kebab-case
-Vue.component('child',{
+Vue.component('child', {
     // 用 props 选项声明它预期的数据。
     // 数据来自哪里？1.直接在html标签中赋值；2.父组件传递下来的？
-    props:['myMessage'],
+    props: ['myMessage'],
     // 就像 data 一样，prop 也可以在模板中使用，同样也可以在 vm 实例中通过 this.message 来使用
-    template:'<span>{{myMessage}}</span>'
+    template: '<p>{{myMessage}}</p>'
+})
+
+// 动态 Prop
+// 
+Vue.component('dynamic-child', {
+    props: ['message'],
+    template: '<h1>{{message}}</h1>'
+})
+
+// 应该传入什么？
+Vue.component('dynamic-child-2', {
+    props: ['todo'],
+    template: '<h1>{{todo}}</h1>'
 })
 
 
@@ -74,7 +88,11 @@ Vue.component('child',{
 var vm = new Vue({
     el: '#app',
     data: {
-
+        parentMsg: 'Message from parent',
+        todo: {
+            text: 'Learning Vue.js',
+            isComplete: false
+        }
     },
     components: {
 
