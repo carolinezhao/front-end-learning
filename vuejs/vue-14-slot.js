cb168fe117c35c2c29f1fab4df4129cfcd9c9bb5
carolinezhao <chaohsimeng@gmail.com>
2018-01-11 23:38:17 +0800

    named slots

--- vuejs/vue-14-slot.js
+++ vuejs/vue-14-slot.js
@@ -11,7 +11,7 @@
 // 除非子组件模板包含至少一个 <slot> 插口，否则父组件的内容将会被丢弃。
 // 当子组件模板只有一个没有属性的插槽时，父组件传入的整个内容片段将插入到插槽所在的 DOM 位置，并替换掉插槽标签本身。
 // 最初在 <slot> 标签中的任何内容都被视为备用内容。备用内容在子组件的作用域内编译，并且只有在宿主元素为空，且没有要插入的内容时才显示备用内容。
-
+// 父组件
 Vue.component('parent-component', {
     template: `<div>
     <h1>我是父组件的标题</h1>
@@ -24,7 +24,7 @@ Vue.component('parent-component', {
         return {}
     }
 })
-
+// 子组件
 Vue.component('child-component', {
     template: `<div>
                <h2>我是子组件的标题</h2>
@@ -34,6 +34,36 @@ Vue.component('child-component', {
         return {}
     }
 })
+
+// 具名插槽
+// <slot> 元素可以用一个特殊的特性 name 来进一步配置如何分发内容。多个插槽可以有不同的名字。具名插槽将匹配内容片段中有对应 slot 特性的元素。
+// 仍然可以有一个匿名插槽，它是默认插槽，作为找不到匹配的内容片段的备用插槽。如果没有默认插槽，这些找不到匹配的内容片段将被抛弃。
+// 在设计组合使用的组件时，内容分发 API 是非常有用的机制。
+// 父组件，模板中的子组件标签可能在最外层！！
+Vue.component('parent-component-2', {
+    template: `<child-component-2>
+    <h1 slot="header">这里可能是一个页面标题</h1> 
+    <p>主要内容的一个段落。</p>
+    <p>另一个主要段落。</p>
+    <p slot="footer">这里有一些联系信息</p>
+  </child-component-2>`
+})
+// 子组件
+Vue.component('child-component-2', {
+    template: `<div class="container">
+    <header>
+      <slot name="header"></slot>
+    </header>
+    <main>
+      <slot></slot>
+    </main>
+    <footer>
+      <slot name="footer"></slot>
+    </footer>
+  </div>`
+})
+
+// 作用域插槽（还没看）
 
 var vm = new Vue({
     el: '#app',
