d2c134316d8c2615b7d830c979b2529f14f0504c
carolinezhao <chaohsimeng@gmail.com>
2018-02-08 16:41:35 +0800

    array.sort using arrow function

--- js/function/es6-arrow-function.js
+++ js/function/es6-arrow-function.js
@@ -1,5 +1,4 @@
-// 箭头函数
-
+/* ============== 箭头函数语法 ==============*/
 // 传入1个参数
 // let reflect = function(value) {
 //     return value
@@ -24,7 +23,7 @@ let diff = (num1, num2) => {
         return num2 - num1
     }
 }
-console.log(diff(6,10))
+console.log(diff(6, 10))
 
 // 空函数
 let doNothing = () => {}
@@ -35,3 +34,19 @@ let getTempItem = id => ({
     name: 'Temp'
 })
 console.log(getTempItem(5))
+console.log('')
+
+/* ============== 创建立即执行函数表达式 ==============*/
+
+
+
+/* ============== 箭头函数和数组 ==============*/
+// var result = values.sort(function (a, b) {
+//     return a - b
+// })
+// 相当于：
+var values = [36, 12, 49]
+var result = values.sort((a, b) => a - b)
+console.log(result)
+
+// sort(), map(), reduce() 这些可以接受回调函数的数组方法，都可以通过箭头函数语法简化编码过程。
