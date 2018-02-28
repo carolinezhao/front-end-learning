4cd70c44473fd665e52ec611a4aa36f1e4bf0ae9
carolinezhao <chaohsimeng@gmail.com>
2018-02-28 22:33:57 +0800

    for loop and closure

--- js/function/know-closure.js
+++ js/function/know-closure.js
@@ -57,15 +57,21 @@ function wait(msg) {
 wait('Hello, closure!')
 console.log('')
 
+
 // 5.4 循环和闭包
 // 延迟函数的回调会在循环结束时才执行，即使时间设为0，依然如此。
+// 因此输出显示的是循环结束时 i 的最终值，每秒一次输出五个6。
+// （以下根据作用域的工作原理 --> 还没看）
+// 尽管循环中的五个函数是在各个迭代中分别定义的，但它们都被封闭在一个共享的全局作用域中(?)，因此实际上只有一个 i。
 for (var i = 1; i <= 5; i++) {
     setTimeout(function timer() {
         console.log(i)
     }, i * 1000)
 }
+// 改进需求：循环的过程中每个迭代都需要一个闭包作用域。
+// IIFE 会通过声明并立即执行一个函数来创建作用域。
 
-// 
+// 如果作用域是空的，那么仅仅将它们封闭是不够的。
 for (var i = 1; i <= 5; i++) {
     (function () {
         setTimeout(function timer() {
@@ -74,17 +80,17 @@ for (var i = 1; i <= 5; i++) {
     })();
 }
 
-// 
+// IIFE 需要实质内容--有自己的变量，用来在每个迭代中储存 i 的值。
 for (var i = 1; i <= 5; i++) {
     (function () {
-        var j=i
+        var j = i
         setTimeout(function timer() {
             console.log(j)
         }, j * 1000)
     })();
 }
 
-// 
+// 改进
 for (var i = 1; i <= 5; i++) {
     (function (j) {
         setTimeout(function timer() {
@@ -92,3 +98,7 @@ for (var i = 1; i <= 5; i++) {
         }, j * 1000)
     })(i);
 }
+
+// 在迭代内使用 IIFE 会为每个迭代都生成一个新的作用域，
+// 使得延迟函数的回调可以将新的作用域封闭在每个迭代内部，
+// 每个迭代中都会含有一个具有正确值的变量供访问。
