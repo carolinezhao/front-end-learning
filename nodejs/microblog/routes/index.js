58a4860dc3f9aca6fd5620b74f73d6e62fa249fb
carolinezhao <chaohsimeng@gmail.com>
2018-03-30 16:28:01 +0800

    register post

--- nodejs/microblog/routes/index.js
+++ nodejs/microblog/routes/index.js
@@ -21,11 +21,45 @@ router.get('/reg', function (req, res) {
 })
 
 router.post('/reg', function (req, res) {
+    // 检查两次输入的密码是否一致
+    if (req.body['password-confirm'] != req.body['password']) {
+        req.flash('error', '两次输入的密码不一致')
+        return res.redirect('/reg')
+    }
 
+    // 生成密码的散列值
+    var md5 = crypto.createHash('md5')
+    var password = md5.update(req.body.password).digest('base64')
+
+    var newUser = new User({
+        name: req.body.username,
+        password: password
+    })
+
+    // 检查用户名是否已经存在
+    User.get(newUser.name, function (err, user) {
+        if (user) {
+            err = 'Username already exists.'
+        }
+        if (err) {
+            req.flash('error', err)
+            return res.redirect('/reg')
+        }
+        // 如果不存在则新增用户
+        newUser.save(function (err) {
+            if (err) {
+                req.flash('error', err)
+                return res.redirect('/reg')
+            }
+            req.session.user = newUser
+            req.flash('success', '注册成功！')
+            res.redirect('/')
+        })
+    })
 })
 
 router.get('/login', function (req, res) {
-
+    res.render('login', {})
 })
 
 router.post('/login', function (req, res) {
@@ -33,7 +67,7 @@ router.post('/login', function (req, res) {
 })
 
 router.get('/logout', function (req, res) {
-
+    res.render('logout', {})
 })
 
 module.exports = router;
