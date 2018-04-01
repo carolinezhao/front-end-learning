845d501156cccf2eeb01680e83fd9e05d65a36f5
carolinezhao <chaohsimeng@gmail.com>
2018-04-02 00:52:10 +0800

    update modules

--- nodejs/microblog/models/db.js
+++ nodejs/microblog/models/db.js
@@ -3,5 +3,8 @@ var Db = require('mongodb').Db
 var Connection = require('mongodb').Connection
 var Server = require('mongodb').Server
 
-module.exports = new Db(settings.db, new Server(settings.host, Connection.DEFAULT_PORT, {}))
+// module.exports = new Db(settings.db, new Server(settings.host, Connection.DEFAULT_PORT, {}))
+// Error: Cannot read property 'DEFAULT_PORT' of undefined
+// 改为 mongodb 的默认端口号 20717
+module.exports = new Db(settings.db, new Server(settings.host, 27017, {}))
 // 输出了创建的数据库连接。
--- nodejs/microblog/settings.js
+++ nodejs/microblog/settings.js
@@ -1,7 +1,5 @@
-var microblog = {
+module.exports = {
     cookieSecret: 'microblogcaroline', // 用于 Cookie 加密，与数据库无关，留作后用。
     db: 'microblog', // 数据库名称
-    host: '127.0.0.1' // 数据库地址
+    host: '127.0.0.1' // 数据库地址 localhost
 }
-
-module.exports = microblog
