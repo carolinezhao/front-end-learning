214f2f9ac6cb4fa1eebcb19add2c0766dac8efc7
carolinezhao <chaohsimeng@gmail.com>
2018-04-03 16:51:56 +0800

    connect to mongodb v3.0

--- nodejs/microblog/models/db-old.js
+++ nodejs/microblog/models/db-old.js
@@ -1,3 +1,5 @@
+// 书中提供的写法，适用版本是 "mongodb": ">= 0.9.9"
+
 var settings = require('../settings')
 var Db = require('mongodb').Db
 var Connection = require('mongodb').Connection
@@ -8,3 +10,8 @@ var Server = require('mongodb').Server
 // 改为 mongodb 的默认端口号 27017
 module.exports = new Db(settings.db, new Server(settings.host, 27017, {}))
 // 输出了创建的数据库连接。
+
+// 相当于拆开写
+// var server = new Server(settings.host, 27017, {});
+// var db = new Db(settings.db, server);
+// module.exports = db;
