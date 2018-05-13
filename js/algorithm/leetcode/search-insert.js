c2beebb26c517554ec51a421864e55455dc64a85
carolinezhao <chaohsimeng@gmail.com>
2018-05-13 22:51:20 +0800

    leetcode: search insert position by binary

--- js/algorithm/leetcode/search-insert.js
+++ js/algorithm/leetcode/search-insert.js
@@ -1,19 +1,20 @@
-// Given a sorted array and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+// Given a sorted array and a target value, return the index if the target is found. 
+// If not, return the index where it would be if it were inserted in order.
 // You may assume no duplicates in the array.
 
-var array = [1, 3, 5, 6],
-    target = 7;
+var array = [1, 3, 5, 6, 9, 10, 12, 20],
+    target = 18;
 
-// 方法1
-var searchInsert = function (nums, target) {
-    var len = nums.length;    
+// 方法1: 顺序查找
+var searchInsert1 = function (nums, target) {
+    var len = nums.length;
     if (target < nums[0]) {
         return 0;
     }
     if (target > nums[len - 1]) {
         return len;
     }
-    for (var i = 0; i < len; i++) {     
+    for (var i = 0; i < len; i++) {
         if (nums[i] === target) {
             return i;
         }
@@ -22,6 +23,34 @@ var searchInsert = function (nums, target) {
         }
     }
 };
-console.log(searchInsert(array, target));
+console.log(searchInsert1(array, target));
+console.log('');
 
 // 方法2: 二分查找
+var searchInsert2 = function (nums, target) {
+    var len = nums.length;
+    if (target < nums[0]) {
+        return 0;
+    }
+    if (target > nums[len - 1]) {
+        return len;
+    }
+    var left = 0,
+        right = len - 1,
+        mid, element;
+    while (right >= left) {
+        mid = Math.floor((left + right) / 2);
+        element = nums[mid];
+        console.log(left, mid, right);
+        
+        if (element > target) {
+            right = mid - 1;
+        } else if (element < target) {
+            left = mid + 1;
+        } else {
+            return mid;
+        }
+    }
+    return left;
+}
+console.log(searchInsert2(array, target));
