5ca665e2603232aa271782f56391b28b19b3a1d5
carolinezhao <chaohsimeng@gmail.com>
2018-09-02 21:46:09 +0800

    review recursion

--- algorithm/recursion.js
+++ algorithm/recursion.js
@@ -1,5 +1,15 @@
 // 递归
 
+function factorial(x) {
+    if (x === 1) {
+        return 1;
+    } else {
+        return x * factorial(x-1);
+    }
+}
+// console.log(factorial(5));
+
+
 // 斐波那契数列：1, 1, 2, 3, 5...
 
 let fibonacci = [1, 1]
@@ -8,14 +18,15 @@ for (let i = 2; i < 20; i++) {
 }
 // console.log(fibonacci.length, fibonacci);
 
-// 1 和 2 的斐波那契数是 1；
-// n (n > 2) 的斐波那契数是 (n-1) 的斐波那契数与 (n-2) 的斐波那契数之和。
-// 这里的 n 相当于索引 (索引 0 为空)。
+// 1 和 2 的斐波那契数是 1；(基线条件)
+// n (n > 2) 的斐波那契数是 (n-1) 的斐波那契数与 (n-2) 的斐波那契数之和。(递归条件)
+// 求第 n 项。
+
 // 递归通常比普通实现慢，但是容易理解，且代码量少。
 let countA = 0 // 记录递归次数
 function fibonacci1(n) {
     countA++;
-    if (n === 1 || n === 2) { // 边界条件
+    if (n === 1 || n === 2) {
         return 1;
     }
     return fibonacci1(n - 1) + fibonacci1(n - 2);
@@ -29,7 +40,7 @@ function fibonacci2(n) {
     let cache = []
 
     function func(n) {
-        console.log('n=' + n, cache);
+        console.log('n=' + n);
         if (cache[n]) {
             console.log('cache' + n);
             return cache[n];
@@ -40,14 +51,14 @@ function fibonacci2(n) {
             return 1;
         }
         let pre = func(n - 1);
-        console.log('n=' + n + ', pre=' + pre);
         cache[n - 1] = pre;
+        console.log('n=' + n + ', pre=' + pre, cache);
         let prepre = func(n - 2);
-        console.log('n=' + n + ', prepre=' + prepre);
         cache[n - 2] = prepre;
+        console.log('n=' + n + ', prepre=' + prepre, cache);
         return pre + prepre;
     }
     return func(n);
 }
-console.log(fibonacci2(20), countB); // 6765 20
-// console.log(fibonacci2(5), countB);
+// console.log(fibonacci2(20), countB); // 6765 20
+console.log(fibonacci2(5), countB);
