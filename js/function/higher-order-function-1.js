98d9e031c6c2fcc2eecf459c4a06cfeedf61e528
carolinezhao <chaohsimeng@gmail.com>
2018-10-16 22:28:50 +0800

    higher-order function exercises

--- js/function/higher-order-function-1.js
+++ js/function/higher-order-function-1.js
@@ -58,8 +58,106 @@ repeat(3, n => {
 ["A", "B"].forEach(l => console.log(l));
 
 
+// test data
+let dataSet = [{
+    name: 'a',
+    city: 'bj',
+    senior: true
+}, {
+    name: 'b',
+    city: 'sh',
+    senior: false
+}, {
+    name: 'c',
+    city: 'sz',
+    senior: true
+}];
+
 // Filtering arrays
+function filterFunc(array, condition) {
+    let passed = [];
+    for (item of array) {
+        if (condition(item)) {
+            passed.push(item);
+        }
+    }
+    return passed;
+}
+
+console.log(filterFunc(dataSet, obj => obj.senior));
+
+// built-in array method
+console.log(dataSet.filter(obj => obj.city === 'sh'));
+
+
 // Transforming with map
+function mapFunc(array, transform) {
+    let mapped = [];
+    for (item of array) {
+        mapped.push(transform(item));
+    }
+    return mapped;
+}
+
+let seniorSet = filterFunc(dataSet, obj => obj.senior);
+console.log(mapFunc(seniorSet, obj => obj.name));
+console.log(mapFunc(dataSet, obj => obj.name.toUpperCase()));
+
+// built-in array method
+console.log(dataSet.map(obj => obj.city));
+
+
 // Summarizing with reduce
+function reduceFunc(array, combine, start) {
+    let current = start;
+    for (item of array) {
+        current = combine(item, current);
+    }
+    return current;
+}
+console.log(reduceFunc([1, 2, 3, 4, 5], (a, b) => a + b, 10));
+
+// built-in array method
+console.log([1, 2, 3, 4, 5].reduce((prev, curr) => prev + curr, 10));
+
 
 // Composability
+// Higher-order functions start to shine when you need to compose operations.
+let team1 = [{
+    show: true,
+    score: 90
+}, {
+    show: false
+}, {
+    show: true,
+    score: 66
+}, {
+    show: true,
+    score: 78
+}];
+
+let team2 = [{
+    show: true,
+    score: 80
+}, {
+    show: true,
+    score: 75
+}, {
+    show: true,
+    score: 82
+}, {
+    show: false
+}]
+
+function average(array) {
+    console.log(array);
+    return array.reduce((a, b) => a + b) / array.length;
+}
+
+function getScore(array) {
+    return array.filter(item => item.show).map(item => item.score)
+}
+
+let average1 = Math.round(average(getScore(team1)));
+let average2 = Math.round(average(getScore(team2)));
+console.log(average1, average2);
